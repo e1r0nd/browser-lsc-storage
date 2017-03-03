2c678ea1b09f4b72dc360b764f9bce8243282e43
e1r0nd <a.merezhanyi@gmail.com>
2017-03-03 09:09:16 +0100

    implemented prefix

--- localstorage.js
+++ localstorage.js
@@ -4,6 +4,7 @@
  * http://linkedin.com/in/merezhany/ a.merezhany@gmail.com
  * Placed in public domain.
  */
+
 export default class BrowserLocalStorageClass {
   constructor(dbName = '') {
     const hasLocalStorage = () => {
@@ -31,12 +32,6 @@ export default class BrowserLocalStorageClass {
       throw new Error('Local Storage is not available.');
     }
     this._prefix = dbName;
-    // Get index
-    // this.index = [];
-    // const strIndex = localStorage.getItem(`${this.name}-words`);
-    // if (strIndex) {
-      // this.index = strIndex.split(',');
-    // }
     this._isOK = true;
   }
 
@@ -44,149 +39,157 @@ export default class BrowserLocalStorageClass {
     return this._isOK;
   }
 
-  readItem(key) {
-    return this.isOK && JSON.parse(localStorage.getItem(key));
+  get prefix() {
+    return this._prefix;
   }
 
-  removeItem(key) {
-    if (this.isOK) {
-      localStorage.removeItem(key);
-    }
+  set prefix(value) {
+    this._prefix = value;
   }
 
-  storeItem(key, value) {
-    if (this.isOK) {
-      try {
-        localStorage.setItem(key, JSON.stringify(value));
-      } catch (err) {
-        if ('QUOTA_EXCEEDED_ERR' === err) {
-          throw new Error('Local Storage is full');
-        }
+  // readItem(key) {
+  //   return this.isOK && JSON.parse(localStorage.getItem(key));
+  // }
 
-        return false;
-      }
+  // removeItem(key) {
+  //   if (this.isOK) {
+  //     localStorage.removeItem(key);
+  //   }
+  // }
 
-      return true;
-    }
+  // storeItem(key, value) {
+  //   if (this.isOK) {
+  //     try {
+  //       localStorage.setItem(key, JSON.stringify(value));
+  //     } catch (err) {
+  //       if ('QUOTA_EXCEEDED_ERR' === err) {
+  //         throw new Error('Local Storage is full');
+  //       }
+  //
+  //       return false;
+  //     }
+  //
+  //     return true;
+  //   }
+  //
+  //   return false;
+  // }
 
-    return false;
-  }
+  // putSettings(theSettingsObj) {
+  //   this.storeItem(`${this.name}-words-settings`, theSettingsObj);
+  // }
 
-  putSettings(theSettingsObj) {
-    this.storeItem(`${this.name}-words-settings`, theSettingsObj);
-  }
+  // getSettings() {
+  //   let settings = this.readItem(`${this.name}-words-settings`);
+  //   if (!settings) {
+  //     // The app runs for the first time, thus
+  //     // Initialize the setting object neeeds to be initialized
+  //     // With default values.
+  //
+  //     // First is for box (or step) 1 in the Leitner box;
+  //     //       Ask the word again after 1 day
+  //     // Second is for box 2 ; ask the word again after 3 days
+  //     // Third is for box 3 ; ask the word again after 7 days
+  //
+  //     // Note: box 0 is for the Learn mode and it not set
+  //     // As the words are accessible all the time
+  //     console.log('initialize settings');
+  //     settings = {
+  //       first: 1,
+  //       second: 3,
+  //       third: 7,
+  //     };
+  //     this.storeItem(`${this.name}-settings`, settings);
+  //     this.storeItem(`${this.name}-language`, 'en_GB');
+  //   }
+  //
+  //   return settings;
+  // }
 
-  getSettings() {
-    let settings = this.readItem(`${this.name}-words-settings`);
-    if (!settings) {
-      // The app runs for the first time, thus
-      // Initialize the setting object neeeds to be initialized
-      // With default values.
+  // loadWords(theWords) {
+  //   let index = 0;
+  //   const arrayOfKeys = [];
+  //   const storeEachElement = (element) => {
+  //     element.index = `index${++index}`;
+  //     element.step = 0;
+  //     element.date = 0;
+  //     this.storeItem(`${this.name}-${element.index}`, element);
+  //     arrayOfKeys.push(element.index);
+  //   };
+  //
+  //   theWords.forEach(storeEachElement.bind(this));
+  //
+  //   this.storeItem(`${this.name}-words`, arrayOfKeys.join());
+  //   this.index = arrayOfKeys;
+  //
+  //   console.log(`${arrayOfKeys.length} words have been loaded`);
+  // }
 
-      // First is for box (or step) 1 in the Leitner box;
-      //       Ask the word again after 1 day
-      // Second is for box 2 ; ask the word again after 3 days
-      // Third is for box 3 ; ask the word again after 7 days
+  // isEmpty() {
+  //   return this.isOK && !this.index.length;
+  // }
 
-      // Note: box 0 is for the Learn mode and it not set
-      // As the words are accessible all the time
-      console.log('initialize settings');
-      settings = {
-        first: 1,
-        second: 3,
-        third: 7,
-      };
-      this.storeItem(`${this.name}-settings`, settings);
-      this.storeItem(`${this.name}-language`, 'en_GB');
-    }
+  // dumpWords() {
+  //   if (this.isOK) {
+  //     let key = '';
+  //     let strValue = '';
+  //     const result = [];
+  //
+  //     const prefixForNumber = `${this.name}-index`;
+  //
+  //     // Go through all keys starting with the name
+  //     // Of the database, i.e 'learnWords-index14'
+  //     // Collect the matching objects into arr
+  //     for (let index = 0; index < localStorage.length; index++) {
+  //       key = localStorage.key(index);
+  //       strValue = localStorage.getItem(key);
+  //
+  //       if (!key.lastIndexOf(prefixForNumber)) {
+  //         result.push(JSON.parse(strValue));
+  //       }
+  //     }
+  //
+  //     // Dump the array as JSON code (for select all / copy)
+  //     console.log(JSON.stringify(result));
+  //   }
+  // }
 
-    return settings;
-  }
+  // removeObjects(aKeyPrefix) {
+  //   if (this.isOK) {
+  //     let key = '';
+  //     const keysToDelete = [];
+  //
+  //     // Go through all keys starting with the name
+  //     // Of the database, i.e 'learnWords-index14'
+  //     for (let index = 0; index < localStorage.length; index++) {
+  //       key = localStorage.key(index);
+  //
+  //       if (!key.lastIndexOf(aKeyPrefix)) {
+  //         keysToDelete.push(key);
+  //       }
+  //     }
+  //     // Now we have all the keys which should be deleted
+  //     // In the array keysToDelete.
+  //     console.log(keysToDelete);
+  //     keysToDelete.forEach((aKey) => {
+  //       localStorage.removeItem(aKey);
+  //     });
+  //   }
+  // }
 
-  loadWords(theWords) {
-    let index = 0;
-    const arrayOfKeys = [];
-    const storeEachElement = (element) => {
-      element.index = `index${++index}`;
-      element.step = 0;
-      element.date = 0;
-      this.storeItem(`${this.name}-${element.index}`, element);
-      arrayOfKeys.push(element.index);
-    };
+  // removeWords() {
+  //   const aKeyPrefix = `${this.name}-index`;
+  //
+  //   this.removeObjects(aKeyPrefix);
+  //   // Reset index
+  //   localStorage.setItem(`${this.name}-words`, '');
+  //   // This one triggers that memorystore is executed
+  //   localStorage.removeItem(`${this.name}-settings`);
+  // }
 
-    theWords.forEach(storeEachElement.bind(this));
-
-    this.storeItem(`${this.name}-words`, arrayOfKeys.join());
-    this.index = arrayOfKeys;
-
-    console.log(`${arrayOfKeys.length} words have been loaded`);
-  }
-
-  isEmpty() {
-    return this.isOK && !this.index.length;
-  }
-
-  dumpWords() {
-    if (this.isOK) {
-      let key = '';
-      let strValue = '';
-      const result = [];
-
-      const prefixForNumber = `${this.name}-index`;
-
-      // Go through all keys starting with the name
-      // Of the database, i.e 'learnWords-index14'
-      // Collect the matching objects into arr
-      for (let index = 0; index < localStorage.length; index++) {
-        key = localStorage.key(index);
-        strValue = localStorage.getItem(key);
-
-        if (!key.lastIndexOf(prefixForNumber)) {
-          result.push(JSON.parse(strValue));
-        }
-      }
-
-      // Dump the array as JSON code (for select all / copy)
-      console.log(JSON.stringify(result));
-    }
-  }
-
-  removeObjects(aKeyPrefix) {
-    if (this.isOK) {
-      let key = '';
-      const keysToDelete = [];
-
-      // Go through all keys starting with the name
-      // Of the database, i.e 'learnWords-index14'
-      for (let index = 0; index < localStorage.length; index++) {
-        key = localStorage.key(index);
-
-        if (!key.lastIndexOf(aKeyPrefix)) {
-          keysToDelete.push(key);
-        }
-      }
-      // Now we have all the keys which should be deleted
-      // In the array keysToDelete.
-      console.log(keysToDelete);
-      keysToDelete.forEach((aKey) => {
-        localStorage.removeItem(aKey);
-      });
-    }
-  }
-
-  removeWords() {
-    const aKeyPrefix = `${this.name}-index`;
-
-    this.removeObjects(aKeyPrefix);
-    // Reset index
-    localStorage.setItem(`${this.name}-words`, '');
-    // This one triggers that memorystore is executed
-    localStorage.removeItem(`${this.name}-settings`);
-  }
-
-  destroy() {
-    const aKeyPrefix = this.name;
-
-    this.removeObjects(aKeyPrefix);
-  }
+  // destroy() {
+  //   const aKeyPrefix = this.name;
+  //
+  //   this.removeObjects(aKeyPrefix);
+  // }
 }
--- test/localstorage.spec.js
+++ test/localstorage.spec.js
@@ -19,43 +19,45 @@ describe('# localStorage', () => {
   });
 });
 
-// describe('localStorage', () => {
-//   it('set prefix', () => {
-//     Local.prefix('abc');
-//     assert.equal(Local.prefix(), 'abc');
-//   });
-// });
+describe('localStorage', () => {
+  it('check prefix', () => {
+    const Local = new LocalClass('abc');
+    Local.prefix = 'qwe';
+    assert.equal(Local.prefix, 'qwe');
+  });
+});
+/*
+describe('localStorage: can search?', () => {
+  it('check for a key', () => {
+    assert.equal(Local.has('a-key'), true);
+  });
+});
 
-// describe('localStorage: can search?', () => {
-//   it('check for a key', () => {
-//     assert.equal(Local.has('a-key'), true);
-//   });
-// });
-//
-// describe('localStorage: can read?', () => {
-//   it('read a key', () => {
-//     assert.equal(Local.key('b-key'), 'b-value');
-//   });
-// });
-//
-// describe('localStorage: can write?', () => {
-//   it('write a key', () => {
-//     Local.key('c-key', 'c-value');
-//     assert.equal(Local.key('c-key'), false);
-//   });
-// });
-//
-// describe('localStorage: can delete?', () => {
-//   it('delete a key', () => {
-//     Local.key('d-key', 'd-value');
-//     assert.equal(Local.has('d-key'), false);
-//   });
-// });
-//
-// describe('localStorage: destroy?', () => {
-//   it('remove all keys', () => {
-//     Local.key('e-key', 'e-value');
-//     Local.destroy();
-//     assert.equal(Local.has('e-key'), false);
-//   });
-// });
+describe('localStorage: can read?', () => {
+  it('read a key', () => {
+    assert.equal(Local.key('b-key'), 'b-value');
+  });
+});
+
+describe('localStorage: can write?', () => {
+  it('write a key', () => {
+    Local.key('c-key', 'c-value');
+    assert.equal(Local.key('c-key'), false);
+  });
+});
+
+describe('localStorage: can delete?', () => {
+  it('delete a key', () => {
+    Local.key('d-key', 'd-value');
+    assert.equal(Local.has('d-key'), false);
+  });
+});
+
+describe('localStorage: destroy?', () => {
+  it('remove all keys', () => {
+    Local.key('e-key', 'e-value');
+    Local.destroy();
+    assert.equal(Local.has('e-key'), false);
+  });
+});
+*/
