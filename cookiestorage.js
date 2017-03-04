9d59f831128036b458cdc3265ab8da68a5233c7c
e1r0nd <a.merezhanyi@gmail.com>
2017-03-04 15:52:28 +0100

    fixed typos

--- cookiestorage.js
+++ cookiestorage.js
@@ -1,10 +1,10 @@
 /**
- * Browser LSC Storage API // sessionstorage.js
- * coded by Anatol Marezhanyi aka e1r0nd//[CRG] - February 2017
+ * Browser LSC Storage API // cookiestorage.js
+ * coded by Anatol Marezhanyi aka e1r0nd//[CRG] - March 2017
  * http://linkedin.com/in/merezhany/ a.merezhany@gmail.com
  * Placed in public domain.
  */
-export default class BrowserCookieStorageClass {
+export default class BrowserCookiesStorageClass {
   constructor() {
     this.OK = true;
   }
