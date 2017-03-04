800faa1ccefae8e2240ee43a968293cb113137ac
e1r0nd <a.merezhanyi@gmail.com>
2017-03-04 15:52:56 +0100

    updated date

--- sessionstorage.js
+++ sessionstorage.js
@@ -1,6 +1,6 @@
 /**
  * Browser LSC Storage API // sessionstorage.js
- * coded by Anatol Marezhanyi aka e1r0nd//[CRG] - February 2017
+ * coded by Anatol Marezhanyi aka e1r0nd//[CRG] - March 2017
  * http://linkedin.com/in/merezhany/ a.merezhany@gmail.com
  * Placed in public domain.
  */
