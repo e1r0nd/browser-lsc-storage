efabbaa6534cef2188411da0ea5865f52e4d2e67
e1r0nd <a.merezhanyi@gmail.com>
2017-03-23 23:03:45 +0100

    update docs

--- cookiesstorage.js
+++ cookiesstorage.js
@@ -1,10 +1,6 @@
 /**
- * Browser LSC Storage API // cookiestorage.js
- * coded by Anatol Marezhanyi aka e1r0nd//[CRG] - March 2017
- * http://linkedin.com/in/merezhany/ a.merezhany@gmail.com
- * Placed in public domain.
+ * Cookies Storage API wrapper class constructor
  */
-
 export default class BrowserCookiesStorageClass {
   constructor() {
     if ('undefined' == typeof document || 'undefined' == typeof document.cookie) {
